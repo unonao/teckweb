1c3f0b4fa9e402317fb465b3b10294d62326c64c
村上直輝 <murakaminaoki@mbp-3.local>
2018-01-29 23:38:26 +0900

    change parallax

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@
 	// Document on load.
 	$(function () {
 	    var ua = navigator.userAgent;
-	    if !((ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0)) {
+	    if (!(ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0)) {
 				parallax();
 	    }
 	});
